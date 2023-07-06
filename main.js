1316e36377278471302275b1cdb7a53e01cec302
Matias <git>
2023-07-06 18:59:34 -0300

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("No website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("Too many command line args");
+    process.exit(1);
+  }
+  for (const arg of process.argv) {
+    console.log(arg);
+  }
+  const baseURL = process.argv[2];
+
+  console.log(`Starting Crawl of ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+main();
