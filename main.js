da445b77b53b8890b07f83e8d790d077addea34c
Matias <git>
2023-07-06 21:13:19 -0300

    crawl page recursive working

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("No website provided");
     process.exit(1);
   }
+
   if (process.argv.length > 3) {
     console.log("Too many command line args");
     process.exit(1);
   }
-  for (const arg of process.argv) {
-    console.log(arg);
-  }
+
   const baseURL = process.argv[2];
 
   console.log(`Starting Crawl of ${baseURL}`);
-  crawlPage(baseURL);
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
